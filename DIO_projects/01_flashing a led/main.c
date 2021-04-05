87f21b0d37ca3a0b25b78619e99bb4b86c1e184d
magdy edwar farid <70660386+Magdyedwar1996@users.noreply.github.com>
2021-04-05 16:14:09 +0200

    this simple project to flash a led

--- DIO_projects/01_flashing a led/main.c
+++ DIO_projects/01_flashing a led/main.c
@@ -17,7 +17,7 @@ int main (void)
 	/*************/
 	// assignment one flashing every 500 ms
 
-	/*DDRB= 0x01 ;
+	DDRB= 0x01 ;
 	while (1)
 	{
 		PORTB = 0X01 ;
@@ -25,46 +25,10 @@ int main (void)
 		PORTB = 0X00 ;
 		_delay_ms(500);
 	}
-*/
 
 
-	/************/
-	/****  2  ****/
-	/*************/
-
-	// assignment two shifting left every 250 ms
 
 
-	DDRC = 0xff ;
-	while (1)
-	{
-		PORTC = 0b00000001 ;
-		_delay_ms(1000);
-
-
-		PORTC = 0b00000010 ;
-		_delay_ms(1000);
-
-		PORTC = 0b00000100 ;
-		_delay_ms(1000);
-
-		PORTC = 0b00001000 ;
-		_delay_ms(1000);
-
-		PORTC = 0b00010000 ;
-		_delay_ms(250);
-
-		PORTC = 0b00100000 ;
-		_delay_ms(1000);
-
-		PORTC = 0b01000000 ;
-		_delay_ms(1000);
-
-		PORTC = 0b10000000 ;
-		_delay_ms(1000);
-
-
-	}
 
 }
 
